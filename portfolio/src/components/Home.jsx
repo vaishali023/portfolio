6de112a8878a18ad89d66e0b1ca5e9ce8f5361e4
vaishali023 <vaishalichaudhary011@gmail.com>
2023-01-22 13:34:31 -0500

    Update Home Component

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -3,17 +3,17 @@ import { HiArrowNarrowRight } from 'react-icons/hi';
 
 const Home = () => {
   return (
-    <div name='home' className=' w-full h-screen bg-[#0a192f]'>
+    <div name='home' className='w-full h-screen bg-[#011826]'>
 
       {/* Container */}
-      <div className='max-w-[1000px] max-auto px-8 flex flex-col justify-center h-full'>
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-pink-600'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Vaishali Chaudhary</h1>
         <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
-          I'm a Full Stack Developer.
+          I'm a FrontEnd Developer.
         </h2>
-            <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a full-stack developer specializzing in building (and occasionally
-            designing) exceptional digital experiences. Currently, I'm focused on building responsive full-stack web applications.</p>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>As a frontend developer, i love to create beautiful
+             and performant web apps with delightful user experiences.</p>
          <div>
             <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
                 View Work
