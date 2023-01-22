66808f2f8105d530dba2fc8d04ede654a0907fdd
vaishali023 <vaishalichaudhary011@gmail.com>
2023-01-22 13:33:12 -0500

    Add content in Work

---
+++ portfolio/src/data/data.js
@@ -0,0 +1,38 @@
+import WorkIm from '../assets/projects/crwn-clothing.png';
+import RealEst from '../assets/projects/crwn-clothing.png';
+import Vagary from '../assets/projects/vagary.png';
+import Movie from '../assets/projects/movie-booking.png';
+import Monsters from '../assets/projects/monsters.png';
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:WorkIm,
+        github:"https://github.com/vaishali023/crwn-clothing",
+        live:"https://cozy-frangollo-0ed914.netlify.app/",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:Vagary,
+        github:"https://github.com/vaishali023/capstone-project",
+        live:"https://amazing-blancmange-33051d.netlify.app/",
+    },
+    {
+        id:3,
+        name:"Typescript Application",
+        image:Monsters,
+        github:"https://github.com/vaishali023/monsters-rolodex",
+        live:"https://stupendous-shortbread-8af5d0.netlify.app/",
+    },
+    {
+        id:4,
+        name:"Vanilla Javascript",
+        image:Movie,
+        github:"https://github.com/vaishali023/movie-seat-booking",
+        live:"https://shimmering-sprite-474d93.netlify.app/",
+    },
+
+
+]
