f09a72a1e7865a153f75b6ca8741c02d8536897f
vaishali023 <vaishalichaudhary011@gmail.com>
2023-01-22 22:55:47 -0500

    Add Logo

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import {FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook, } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import {BsFillPersonLinesFill} from 'react-icons/bs';
-import Logo from '../assets/logo1.png';
+import Vclogo from '../assets/v.png';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>  
     <div>
-        <img src={Logo} alt="Logo Image" style ={{width: '50px'}}/>
+        <img src={Vclogo} alt="Logo Image" style ={{width: '70px', height: '70px'}}/>
     </div>
 
     
